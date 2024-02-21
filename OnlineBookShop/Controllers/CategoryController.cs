4c87f91efbdd10c0c22d315257bd51c643917624
jason <jason>
2024-02-21 21:45:29 +0200

    section 40 - 53

--- OnlineBookShop/Controllers/CategoryController.cs
+++ OnlineBookShop/Controllers/CategoryController.cs
@@ -1,12 +1,14 @@
 ﻿using OnlineBookShop.data;
 using OnlineBookShop.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace OnlineBookShop.Controllers
 {
     public class CategoryController : Controller
     {
         private readonly ApplicationDbContext _db;
+        //Read
         public CategoryController(ApplicationDbContext db)
         {
             _db = db;
@@ -16,6 +18,8 @@ namespace OnlineBookShop.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        //Create
         public IActionResult Create()
         {
             return View();
@@ -23,9 +27,77 @@ namespace OnlineBookShop.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The display order can't match the Name");
+            }
+            if (obj.Name == "test")
+            {
+                ModelState.AddModelError("","Test is an invalid value");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category edited succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        //Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
-            return RedirectToAction("index");
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- OnlineBookShop/Models/Category.cs
+++ OnlineBookShop/Models/Category.cs
@@ -9,8 +9,10 @@ namespace OnlineBookShop.Models
         public  int Id { get; set; }
         [Required]
         [DisplayName("Category Name")]
+        [MaxLength(30, ErrorMessage ="Catogry Name must be less than 30 characters")]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
